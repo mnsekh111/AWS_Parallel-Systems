6cde6f14e1562e34ea2b0a873eb0833906ff00c7
mnsekh111 <mnsekh111@gmail.com>
2016-03-26 14:25:01 -0400

    Working TopN

--- TopN.java
+++ TopN.java
@@ -1,4 +1,5 @@
 
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -11,24 +12,26 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
-
 import java.util.*;
 
 public class TopN {
 
-    public static int N=10; // Default
+    public static int N=10; // Default 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-        if (otherArgs.length != 3) {
-            System.err.println("Usage: TopWord <in> <out> <N>");
-            System.exit(3);
-            TopN.N=Integer.parseInt(otherArgs[2]);
-        }
-
+        
+         
+           
+        Configuration conf = new Configuration();                                                                         
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();                                     
+        if (otherArgs.length != 3) {                                                                                      
+            System.err.println("Usage: TopWord <in> <out> <N>");                                                          
+            System.exit(3);                                                                                               
+            TopN.N=Integer.parseInt(otherArgs[2]);                                                                        
+        }                                                                                                                 
+                                                                                                                          
         /************** Add your code below****************/
 
-        Job job = Job.getInstance(conf, "word count top N");
+        Job job = Job.getInstance(conf);
         job.setJarByClass(TopN.class);
         job.setMapperClass(TopNMapper.class);
         job.setCombinerClass(TopNReducer.class);
@@ -40,70 +43,94 @@ public class TopN {
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-
+        
+        
     }
 
+ 
     public static class TopNMapper extends Mapper<Object, Text, Text, IntWritable> {
 
-        /************** Add your mapper code below****************/
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
+    
 
-         @Override
-         public void map(Object key, Text value, Context context) throws 
-                IOException, InterruptedException {
-      
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
                 StringTokenizer itr = new StringTokenizer(value.toString());
                 while (itr.hasMoreTokens()) {
                         word.set(itr.nextToken());
                         context.write(word, one);
                 }
         }
-
     }
+
 
     public static class TopNReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-        /************** Add your reducer code below****************/
+        private Map<Text, IntWritable> map = new HashMap<>();
 
-        private Map<Text,IntWritable> countMap = new HashMap<>();
         @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws
-                IOException, InterruptedException {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 
-                int sum = 0;
-                for (IntWritable val : values) {
-                        sum += val.get();
-                }
-                
-                countMap.put(key,new IntWritable(sum));
+            // computes the number of occurrences of a single word
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
 
+            map.put(new Text(key), new IntWritable(sum));
         }
 
         @Override
-        protected void cleanup (Context context) throws IOException,InterruptedException{
-                
-                Set<Map.Entry<Text,IntWritable>> set = countMap.entrySet();
-                List<Map.Entry<Text, IntWritable>> list = new ArrayList<Map.Entry<Text,IntWritable>>(set);
-                Collections.sort(list , new Comparator<Map.Entry<Text, IntWritable>>(){
-                    public int compare (Map.Entry<Text,IntWritable> o1, Map.Entry<Text,IntWritable> o2) {
-                        return (o2.getValue()).compareTo (o1.getValue());
-                    }
-                });
+        protected void cleanup(Context context) throws IOException, InterruptedException {
 
-                int counter = 0;
+            Map<Text, IntWritable> sortedMap = sortByValueUtil(map);
 
-                for(Map.Entry<Text,IntWritable> entry : list){
-                    if(counter == TopN.N-1)
-                        break;
-                    context.write(entry.getKey(),entry.getValue());
-                    counter++;
+            int counter = 0;
+            for (Text key : sortedMap.keySet()) {
+                if (counter++ == TopN.N) {
+                    break;
                 }
+                context.write(key, sortedMap.get(key));
+            }
         }
-
     }
 
-   // hadoop jar ~/TopN.jar TopN /user/shakespeare/t8.shakespeare.txt /user/wordcount/topnoutput
 
+    public static class TopNCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+            // computes the number of occurrences of a single word
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+
+    private static <K extends Comparable, V extends Comparable> Map<K, V> sortByValueUtil(Map<K, V> map) {
+        List<Map.Entry<K, V>> entries = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+
+        Collections.sort(entries, new Comparator<Map.Entry<K, V>>() {
+
+            @Override
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+
+        Map<K, V> sortedMap = new LinkedHashMap<K, V>();
+
+        for (Map.Entry<K, V> entry : entries) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedMap;
+    }
 
 }
